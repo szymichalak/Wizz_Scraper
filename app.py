586c376021aae805480a03f2ca68516a549173b2
inf136769 <szymonmichalak97@gmail.com>
2020-03-30 23:55:18 +0200

    add chrome browser and fix iteration over months

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from scraper.scraper import Scraper
 
 
 def main(browser_name):
-    origin = input("Enter city of departure: ")
-    destination = input("Enter city of arrival: ")
+    origin = "Vienna"  # input("Enter city of departure: ")
+    destination = "Tuzla"  # input("Enter city of arrival: ")
+    some_or_whole = input("Would you like to scrap some months or a whole year (some / whole)? ")
+    if some_or_whole == "some":
+        first_month = input("Enter first month: ")
+        second_month = input("Enter second month: ")
+        whole = False
+    else:
+        first_month = None
+        second_month = None
+        whole = True
+
     new_scraper = Scraper(browser_name)
-    new_scraper.scrap_some_months(origin, destination, "May", "July")
+    new_scraper.scrap_prices(origin, destination, whole, first_month, second_month)
     print(new_scraper.calculate_average("first"))
     new_scraper.print_calendar("first")
     time.sleep(10)
@@ -14,5 +24,5 @@ def main(browser_name):
 
 
 if __name__ == "__main__":
-    your_browser = input("What browser do you have (Chrome / Firefox)? ")
+    your_browser = "Firefox"  # input("What browser do you have (Chrome / Firefox)? ")
     main(your_browser)
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -11,7 +12,6 @@ from extra_data.months import months
 
 class Scraper:
     def __init__(self, browser_name):
-        self.url = "https://wizzair.com/en-gb/flights/timetable#/"
         if browser_name == "Firefox":
             driver_path = str(Path().absolute()) + "/scraper/geckodriver"
             self.browser = webdriver.Firefox(executable_path=driver_path)
@@ -19,14 +19,17 @@ class Scraper:
             driver_path = str(Path().absolute()) + "/scraper/chromedriver"
             self.browser = webdriver.Chrome(driver_path)
         self.delayer = Delayer(self.browser)
+        self.url = "https://wizzair.com/en-gb/flights/timetable#/"
         self.browser.get(self.url)
+        self.browser.maximize_window()
         self.first_way_prices = []
         self.return_prices = []
+        self.current_date = datetime.date.today()
 
     def close_browser(self):
         self.browser.quit()
 
-    def scrap_some_months(self, origin, destination, start, stop):
+    def scrap_prices(self, origin, destination, whole_year, start, stop):
         # wait for filling
         self.delayer.clickable(By.ID, "search-departure-station")
 
@@ -46,12 +49,39 @@ class Scraper:
         # click search button
         self.browser.find_element_by_xpath(locators.xpath.SEARCH_BUTTON).click()
 
-        start_month = int(months.get(start))
-        stop_month = int(months.get(stop)) + 1
-        for month_num in range(start_month, stop_month):
+        # calculate number of iterations
+        if whole_year:
+            iterations = 12
+        else:
+            try:
+                start_month = int(months.get(start))
+                stop_month = int(months.get(stop))
+            except Exception:
+                print("Wrong months")
+                return False
+            if start_month == stop_month:
+                iterations = 1
+            elif start_month > stop_month:
+                iterations = 12 - (start_month - stop_month) + 1
+            else:
+                iterations = stop_month - start_month + 1
+
+        for month_num in range(iterations):
+            # wait for months dropdown
             self.delayer.clickable(By.XPATH, locators.xpath.MONTHS_DROPDOWN)
             select_month = Select(self.browser.find_element_by_xpath(locators.xpath.MONTHS_DROPDOWN))
-            select_month.select_by_value("2020-0" + str(month_num))
+
+            # calculate next month which we will scraped
+            if whole_year:
+                selected_month = self.current_date.month + month_num
+            else:
+                selected_month = start_month + month_num
+            selected_year = self.current_date.year
+            if selected_month > 12:
+                selected_month -= 12
+                selected_year += 1
+            selected_value = str(selected_year) + "-" + str(selected_month).zfill(2)
+            select_month.select_by_value(selected_value)
 
             self.delayer.presence(By.CLASS_NAME, "fare-finder__calendar__days__list")
             two_ways_prices = self.browser.find_elements_by_class_name("fare-finder__calendar__days__list")
@@ -78,9 +108,12 @@ class Scraper:
                 except Exception as e:
                     print(e)
                     #  here add some log
+        return True
 
     def calculate_average(self, way):
         if way == "first":
+            if len(self.first_way_prices) == 0:
+                return None
             sum_prices = 0
             not_none = 0
             for day_info in self.first_way_prices:
@@ -90,6 +123,8 @@ class Scraper:
             return sum_prices / not_none
 
         elif way == "return":
+            if len(self.return_prices) == 0:
+                return None
             sum_prices = 0
             not_none = 0
             for day_info in self.return_prices:
